1a0d3fb4452f38e9d8f8229c747e7aa3c39f074b
emgiezet <maxmalecki@gmail.com>
2013-06-05 12:03:10 +0200

    twig extension fix

--- Twig/CampaignExtension.php
+++ Twig/CampaignExtension.php
@@ -71,26 +71,24 @@ class CampaignExtension extends \Twig_Extension implements ContainerAwareInterfa
     {
         $campaign = $this->get($code, $options);
 
-        if($campaign) {
-            $html = '<div id="carousel" style="width:710px; height: 270px;overflow: hidden;">';
-            //return $campaign->getContent();
-            
-            foreach($campaign->getAds() as $ad) {
+        if ($campaign) {
+            $html = '';
+            foreach ($campaign->getAds() as $ad) {
                 $media = $ad->getImage();
-                
-                $mediaservice = $this->container->get('sonata.media.pool');
-                $provider = $mediaservice->getProvider($media->getProviderName());
-                $format = $provider->getFormatName($media, 'big');
-                
-                
-                $html .= '<div class="slide">';
-                $html .= '<a href="' . $ad->getLink() . '">';
-                $html .= '<img src="' . $provider->generatePublicUrl($media, $format) .  '" width="710" height="270">';
-                $html .= '</a>';
-                $html .= '</div>';
+                if (!empty($media)) {
+                    
+                    $mediaservice = $this->container->get('sonata.media.pool');
+                    $provider = $mediaservice->getProvider($media->getProviderName());
+
+                    $format = $provider->getFormatName($media, 'samall');
+                    
+                    $html .= '<div class="item">';
+                    $html .= '<a href="' . $ad->getLink() . '" target="_blank">';
+                    $html .= '<img src="'.$provider->generatePublicUrl($media, 'reference').'" alt="'.$ad->getLink().'" title="doink" >';
+                    $html .= '</a>';
+                    $html .= '</div>';
+                }
             }
-            
-            $html .= '</div>';
             
             return $html;
         }
